3982dc30deebebc0ab6dc2563484cb6085c881b8
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-11 10:35:34 -0800

    changes

--- itis_sqlite_init.R
+++ itis_sqlite_init.R
@@ -1,5 +1,5 @@
 #' Initiate connection to sqlite3 database
-#' 
+#'
 #' @import RSQLite DBI
 #' @param path Path to the sqlite database
 #' @examples \dontrun{
@@ -7,10 +7,10 @@
 #' }
 sqlite_init <- function(path = "~/github/ropensci/sql/itis2.sqlite")
 {
-#   if(!inherits(conn, what="SQLiteConnection"))
-    m <- dbDriver("SQLite")
-    conn <- dbConnect(m, dbname = path)
+  #   if(!inherits(conn, what="SQLiteConnection"))
+  m <- dbDriver("SQLite")
+  conn <- dbConnect(m, dbname = path)
   options(conn = conn)
-    return( conn )
+  return( conn )
 }
 
